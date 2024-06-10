6b9fb2620566e36a6c5c448e031a769ce85e2f70
Simon Wisdom <simonowisdom@gmail.com>
2024-06-10 17:59:36 -0600

    her

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# Navigate to the frontend directory and build the React app
+# Install backend dependencies
+pip install -r backend/requirements.txt
+
+# Navigate to the frontend directory and install dependencies
 cd frontend
 npm install
+
+# Build the React app
 npm run build
 
 # Move the build files to the backend static directory
 cp -a dist/. ../backend/static/
 
-# Navigate back to the root
+# Navigate back to the root directory
 cd ..
