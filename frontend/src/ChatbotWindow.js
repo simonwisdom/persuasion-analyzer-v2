c8e9cdf16d5dbbd73ea8b9662fe1299093675202
Simon Wisdom <simonowisdom@gmail.com>
2024-06-10 13:09:14 -0600

    removed react-icons

--- frontend/src/ChatbotWindow.js
+++ frontend/src/ChatbotWindow.js
@@ -1,5 +1,17 @@
 import React, { useEffect, useRef } from 'react';
-import { FaRobot, FaUser } from 'react-icons/fa';
+
+const RobotIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-blue-500 mr-2 self-end w-4 h-4">
+    <path d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z" />
+  </svg>
+);
+
+const UserIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-green-500 ml-2 self-end w-4 h-4">
+    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+    <circle cx="12" cy="7" r="4" />
+  </svg>
+);
 
 const ChatbotWindow = ({ messages, onNextMessage }) => {
   const messageListRef = useRef(null);
@@ -25,9 +37,7 @@ const ChatbotWindow = ({ messages, onNextMessage }) => {
               msg.type === 'user' ? 'justify-end' : 'justify-start'
             }`}
           >
-            {msg.type === 'persuader' && (
-              <FaRobot className="text-blue-500 mr-2 self-end" />
-            )}
+            {msg.type === 'persuader' && <RobotIcon />}
             <div
               className={`${
                 msg.type === 'user' ? 'bg-green-500' : 'bg-blue-500'
@@ -35,9 +45,7 @@ const ChatbotWindow = ({ messages, onNextMessage }) => {
             >
               {msg.content}
             </div>
-            {msg.type === 'user' && (
-              <FaUser className="text-green-500 ml-2 self-end" />
-            )}
+            {msg.type === 'user' && <UserIcon />}
           </div>
         ))}
       </div>
