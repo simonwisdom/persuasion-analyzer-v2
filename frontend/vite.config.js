44ff14dbdf1f6e06d203c63d2bd7f6958943aaec
james-julius <jamesdarby7@gmail.com>
2024-06-10 15:23:22 -0700

    fix: switch to vite, switch to jsx

---
+++ frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: { "@": "/src" },
+  },
+
+  plugins: [react()],
+
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+//   publicDir: 'public',
+
+  build: {
+    sourcemap: true
+  }
+});
